70c11feccd1b61fc2802e58ee7722d2cdf50560f
Ricardo Rocha <rcarocha@gmail.com>
2018-09-26 08:46:58 -0300

    initial version

---
+++ labs/02-pf-ambiente-stack.hs
@@ -0,0 +1,28 @@
+module LabAmbiente where
+
+--Tarefa 1: Implemente uma funcao que determine se uma lista eh
+--          um palindromo: possui os mesmos elementos quando
+--          avaliada de tras para frente.
+--          Use filter para criar uma versao que encontra um palindromo
+--          em uma frase, ignorando os espacos (ePalindromoF).
+-- ePalindromo "madamimadam" => True
+-- ePalindromo [1,2,4,8,16,8,4,2,1] => True
+-- ePalindromoF "SOCORRAM ME SUBI NO ONIBUS EM MARROCOS" => True
+
+-- Tarefa 2: Implemente uma funcao que elimine os elementos duplicados
+--             consecutivos em uma lista
+-- comprimeL "aaabbbc" => "abc"
+-- comprimeL "aaabbbca" => "abca"
+
+-- Tarefa 3: Duplique os elementos de uma lista
+-- duplica [3, 5, 6] => [3,3,5,5,6,6]
+
+-- Tarefa 4: Replique os elementos de uma lista n vezes
+-- replique 3 [3,5,6] => [3,3,3,5,5,5,6,6,6]
+
+-- Tarefa 5: Remova um elemento da lista a cada n elementos
+-- removeN [1,2,3,4,5,6,7,8,9,10] 3 => [1,2,4,5,7,8,10]
+
+-- Tarefa 6: Implemente o algoritmo do Quicksort, com ajuda do professor.
+
+main = putStr ""
